84a6ffd91590d0f3e548185d5bdbeff91c0ce6fe
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-07-08 12:19:06 +0530

    Add project files.

---
+++ Generator/ToStringGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Generator;
+
+[Generator]
+public class ToStringGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var classes = context.SyntaxProvider
+             .CreateSyntaxProvider(
+                 predicate: static(node, _) => node is ClassDeclarationSyntax,
+                 transform: static(ctx, _) => (ClassDeclarationSyntax)ctx.Node);
+
+        //.Where(c => c.AttributeLists.Count > 0);
+        context.RegisterSourceOutput(classes,static (ctx, source) => Execute(ctx, source));
+    }
+
+    private static void Execute(SourceProductionContext context, ClassDeclarationSyntax classDeclarationSyntax)
+    {
+        var className = classDeclarationSyntax.Identifier.Text;
+        var fileName = $"{className}.g.cs";
+
+        context.AddSource(fileName, "//Generated file !");
+    }
+}
+
---
+++ WireBrainCoffee/Model/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WireBrainCoffee.Model
+{
+    public class Person
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"FirstName:{FirstName}; LastName:{LastName}";
+        }
+    }
+}
